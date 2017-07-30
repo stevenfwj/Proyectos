aa7af763366b8c189aa1fe25121a3845f737d447
juanfalquez97 <30575153+juanfalquez97@users.noreply.github.com>
2017-07-30 13:27:54 -0500

    Vector

---
+++ SGAUpse/Tommy_Proyectos/Lab_Sim/Lab_Sim/Vectores.h
@@ -0,0 +1,31 @@
+#ifndef VECTORES_H_
+#define VECTORES_H_
+
+#include <iostream>
+#include <string.h>
+#include <array>
+
+
+using namespace std;
+
+class Vectores
+{
+private:
+
+	int a;
+	int b;
+	int c[1][1];
+
+
+public:
+	Vectores();
+	
+	int Vec();
+	void setFilCol(int x, int y);
+	int getFilasColumnas();
+	int getVec();
+	~Vectores();
+};
+
+#endif 
+
